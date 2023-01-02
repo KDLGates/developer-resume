035de9655f7a5a45f964f2a7d6df744424ecc96f
Kristopher Gates <kdlgates@kdlgates.com>
2023-01-02 12:44:46 -0500

    Restored client rendering (ref. hydration branch)

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
-import useLocalStorage from 'use-local-storage';
-// import "@fontsource/eb-garamond";
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import useLocalStorage from "use-local-storage";
 import "lato-font";
 
-
-// import { ClientOnly } from 'react-client-only';
+import { ClientOnly } from "react-client-only";
 
 export default function Home() {
-  const [theme, setTheme] = useLocalStorage('theme', 'offwhite');
+  const [theme, setTheme] = useLocalStorage("theme", "offwhite");
 
   const switchTheme = (color) => {
     setTheme(color);
   };
 
   return (
-    // <ClientOnly>
+    <ClientOnly>
       <div id="trunk" data-theme={theme}>
         <Header theme={theme} switchTheme={switchTheme} />
         <Main />
         <Footer />
       </div>
-    // </ClientOnly>
+    </ClientOnly>
   );
 }
